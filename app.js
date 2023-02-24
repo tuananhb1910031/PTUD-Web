9409a9e9efbcd36080fc7d834300b4fade66eb30
tuananhb1910031 <101075790+tuananhb1910031@users.noreply.github.com>
2023-02-24 15:20:53 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -14,4 +14,13 @@ app.get("/", (req, res) => {
 
 app.use("/api/contacts", contactsRouter);
 
+   // handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 module.exports = app;
